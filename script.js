31bad392c26242e73eb8a7bb1cb47c1bf3177140
Julczan <julek111222h@gmail.com>
2025-10-07 10:28:38 +0200

    append and prepend functions

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-class linkedList {}
+class linkedList {
+  constructor() {
+    this.firstNode = null;
+  }
+  append(value) {
+    if (this.firstNode === null) this.prepend(value);
+    else {
+      this.tmp = this.firstNode;
+      while (this.tmp.nextNode !== null) {
+        this.tmp = this.tmp.nextNode;
+      }
+      this.tmp.nextNode = new Node(value, null);
+    }
+  }
+  prepend(value) {
+    this.firstNode = new Node(value, this.firstNode);
+  }
+  head() {
+    return this.firstNode;
+  }
+}
 
 class Node {
-  constructor(value, nextNode) {
+  constructor(value = null, nextNode = null) {
     this.value = value;
     this.nextNode = nextNode;
   }
 }
+
+const list = new linkedList();
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+console.log(list.head());
